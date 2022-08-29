650fb0281cb274eb33defb51c25de46bf3b5b3c3
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2022-08-29 11:56:32 -0300

    add: ienumerator to deal with istrigger and iskinecmatic | add: opacity when player is damaged

--- Assets/Scripts/Player/PlayerHealthSystem.cs
+++ Assets/Scripts/Player/PlayerHealthSystem.cs
@@ -9,22 +9,62 @@ public class PlayerHealthSystem : BaseHealthSystem
 
     public HealthStatusBar healthStatusBar;
 
+    SpriteRenderer sprite;
+    Color defaultColor;
+    Color opacityColor;
+
     BoxCollider2D collider;
     Rigidbody2D rb;
+    Animator anim;
+
+    bool onDamage;
 
     private void Awake()
     {
         healthStatusBar.UpdateHealthStatusBar(health);
         collider = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        sprite = GetComponent<SpriteRenderer>();
+        defaultColor = new Color(1, 1, 1, 1);
+        opacityColor = new Color(1, 1, 1, 0.5f);
     }
 
     public override void OnDamage(object sender, OnDamageSystem.DamageEventArgs args)
     {
         health -= args.damage;
         healthStatusBar.UpdateHealthStatusBar(health);
+        onDamage = true;
+        StartCoroutine("DamageEffect");
+        //stay untouchble for x seconds while this opacity is higher than normal
+    }
+
+    private void Update()
+    {
+        if (onDamage)
+        {
+            sprite.color = opacityColor;
+        }
+        else
+        {
+            sprite.color = defaultColor;
+        }
+    }
+
+    IEnumerator DamageEffect()
+    {
         rb.isKinematic = true;
         collider.isTrigger = true;
-        //stay untouchble for x seconds while this opacity is higher than normal
+
+        // sprite.color
+        
+        anim.SetTrigger("damage");
+        yield return new WaitForSeconds(1.2f);
+        
+        collider.isTrigger = false;
+        rb.isKinematic = false;
+
+        onDamage = false;
     }
 }
