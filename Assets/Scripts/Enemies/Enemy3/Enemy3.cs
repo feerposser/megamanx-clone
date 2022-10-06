c0a3a4bd743621c99f6a2e5f137e7d96ec76b6f4
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2022-10-06 11:58:22 -0300

    add: Enemy 3 EnemyState + aproach to the player position + check distance to the player position + reduce velocity when close to the player position + stop when near to player position

--- Assets/Scripts/Enemies/Enemy3/Enemy3.cs
+++ Assets/Scripts/Enemies/Enemy3/Enemy3.cs
@@ -4,42 +4,133 @@ using UnityEngine;
 
 public class Enemy3 : MonoBehaviour
 {
+    /*
+    APROACH: GOING INTO THE PLAYER POSITION
+    PREPARETOSTOP: AFTER APROACHSTATE, STOP TO PREPARE TO ATTACK
+    STOP: STOP MOVING
+    PREPAREATTACK: GET THE ACTUAL PLAYER POSITION TO ATTACK
+    ATTACK: ATTACK THE PLAYER LAST POSITION
+    REPOSITION: AFTER ATTACKING, MOVE WAY AND START ALL OVER AGAIN
+     */
+    public enum EnemyState { APROACH, PREPARETOSTOP, STOP, PREPAREATTACK, ATTACK, REPOSITION  }
+
     Rigidbody2D rb;
 
-    [SerializeField] float speed;
+    Transform playerPosition;
 
-    public bool isRight;
+    [SerializeField] EnemyState enemyState;
+    [SerializeField] float speed;
+    [SerializeField] float stopDistance1;
+    [SerializeField] float stopDistance2;
+    [SerializeField] float stopTime;
 
     void Start()
     {
-        isRight = false;
+        enemyState = EnemyState.APROACH;
         rb = GetComponent<Rigidbody2D>();
+        playerPosition = GameObject.Find("Player").transform;
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        ExecuteMovement();
+        if (enemyState.Equals(EnemyState.APROACH)) ComputeAproach();
+
+        if (enemyState.Equals(EnemyState.PREPARETOSTOP)) ComputePrepareToStop();
+
     }
 
-    void ExecuteMovement()
+    private void FixedUpdate()
     {
-        if (!isRight)
+        if (enemyState.Equals(EnemyState.APROACH))
         {
-            rb.velocity = Vector2.left * speed;
+            ExecuteAproach();
+        } 
+        
+        if (enemyState.Equals(EnemyState.PREPARETOSTOP))
+        {
+            ExecutePrepareToStop();
+        } 
+        
+        if (enemyState.Equals(EnemyState.PREPAREATTACK))
+        {
+            PrepareToAttack();
         }
-        else
+
+        if (enemyState.Equals(EnemyState.ATTACK))
         {
-            rb.velocity = Vector2.right * speed;
+            Attack();
+        }
+        
+    }
+
+    private void ComputeAproach()
+    {
+        if (StopDistance() == 1)
+        {
+            enemyState = EnemyState.PREPARETOSTOP;
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void ExecuteAproach()
     {
-        string tag = collision.gameObject.tag;
+        MoveToPosition(playerPosition.position - transform.position);
+    }
 
-        if (tag == "Player")
+    private void ComputePrepareToStop()
+    {
+        if (StopDistance() == 2)
         {
-            Debug.Log("set damage to player");
+            enemyState = EnemyState.STOP;
+            StartCoroutine(Stop());
         }
+    }
+
+    private void ExecutePrepareToStop()
+    {
+        MoveToPosition(playerPosition.position - transform.position, 0.7f);
+    }
+ 
+
+    private IEnumerator Stop()
+    {
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(stopTime);
+        enemyState = EnemyState.PREPAREATTACK;
+    }
+
+    private void PrepareToAttack()
+    {
+        lastPlayerPosition = GetActualPlayerPosition();
+        enemyState = EnemyState.ATTACK;
+    }
+
+    private void Attack()
+    {
+        MoveToPosition(playerPosition.position - transform.position);
+    }
+
+    private void MoveToPosition(Vector2 position, float multiplier=1)
+    {
+        rb.velocity = position * multiplier;
+    }
+
+    private int StopDistance()
+    {
+        float distance = Vector2.Distance(playerPosition.position, transform.position);
+
+        if (distance < stopDistance2)
+        {
+            return 2;
+        } else if (distance < stopDistance1)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private Vector2 GetActualPlayerPosition()
+    {
+        return playerPosition.position;
     }
 }
