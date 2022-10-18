d0d1079099e2d0e2e56cb789bb399177b3817f06
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2022-10-17 22:21:13 -0300

    fix: Enemy 7 recreated in GIMP and reorganized in sprite editor

---
+++ Assets/Scripts/Enemies/Enemy7 Crusher/Enemy7.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy7 : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+}
---
+++ Assets/Scripts/Enemies/Enemy7 Crusher/Enemy7HealthSystem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LifeSystem;
+
+public class Enemy7HealthSystem : EnemyHealthSystem
+{
+    
+}
