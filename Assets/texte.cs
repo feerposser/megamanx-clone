130947ec3fd6611fe1c5c77ac9fbbf000c71fc6c
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2022-11-09 11:04:27 -0300

    bug fix on creating a impact in different order sides

--- Assets/texte.cs
+++ Assets/texte.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class texte : MonoBehaviour
 {
@@ -10,19 +11,21 @@ public class texte : MonoBehaviour
 
     Vector2 lastNode;
 
-    Vector3 detection;
-    Vector3 detection2;
+    Vector2 detection;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
 
     void Start()
     {
         p = GetComponent<PolygonCollider2D>();
         
-        OriginalPolygon();
-    }
-
-    Vector2 Create2DVector(float x, float y)
-    {
-        return new Vector2(x, y);
+        GetOriginalPollygonColliderNodes();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -44,73 +47,41 @@ public class texte : MonoBehaviour
 
         destroyGameObject = false;
 
-        Vector2 topRight = new Vector2(v1.x / transform.localScale.x, .5f);
-        Vector2 bottomRight = new Vector2(v1.x / transform.localScale.x, v1.y - 0.3f);
+        Vector2 topLeft = new Vector2(v1.x / transform.localScale.x, .5f);
+        Vector2 bottomLeft = new Vector2(v1.x / transform.localScale.x, v1.y - 0.3f);
 
-        Vector2 bottomLeft = new Vector2(v2.x / transform.localScale.x, v2.y - 0.3f);
-        Vector2 topLeft = new Vector2(v2.x / transform.localScale.x, .5f);
+        Vector2 bottomRight = new Vector2(v2.x / transform.localScale.x, v2.y - 0.3f);
+        Vector2 topRight = new Vector2(v2.x / transform.localScale.x, .5f);
 
-        Vector2[] nodes = { topRight, bottomRight, bottomLeft, topLeft };
+        Vector2[] nodes = { topLeft, bottomLeft, bottomRight, topRight };
 
         if (topLeft.x < lastNode.x)
         {
             for (int i = 0; i < nodes.Length; i++)
             {
-                Debug.Log("<" + points.Count);
                 points.Insert(points.Count - i, nodes[i]);
                 lastNode = nodes[i];
             }
-        } else
+        }
+        else
         {
-            for (int i = 0; i < nodes.Length; i++) //(int i = nodes.Length-1; i >= 0; i--)
+            for (int i = 0; i < nodes.Length; i++)
             {
-                Debug.Log(">" + nodes[i]);
                 points.Insert(points.IndexOf(lastNode), nodes[i]);
                 lastNode = nodes[i];
             }
         }
 
-        /*points.Insert(points.Count, topRight);
-        points.Insert(points.Count, bottomRight);
-        points.Insert(points.Count, bottomLeft);
-        points.Insert(points.Count, topLeft);*/
-
         p.SetPath(0, points);
+
+        Debug.Log(lastNode);
+        detection = lastNode;
 
         destroyGameObject = true;
         return;
-
-        /*for (int i = 0; i < points.Count; i++)
-        {   
-            // x collision point divided by x local scale (same size of the polygon collider) to fit into the polygon collider
-            // increase of decrease x and y to increate the size of the hole
-
-            Vector2 topRight = new Vector2(v1.x / transform.localScale.x, .5f);
-            Vector2 bottomRight = new Vector2(v1.x / transform.localScale.x, v1.y - 0.3f);
-
-            Vector2 bottomLeft = new Vector2(v2.x / transform.localScale.x, v2.y - 0.3f);
-            Vector2 topLeft = new Vector2(v2.x / transform.localScale.x, .5f);
-
-            if (points[i].y >= topLeft.y && points[i].x >= topLeft.x)
-            {
-                points.Insert(i+1, topRight);
-                points.Insert(i+1, bottomRight);
-                points.Insert(i+1, bottomLeft);
-                points.Insert(i+1, topLeft);
-
-                if (topLeft.x < lastNode.x) Debug.Log("<<>>");
-
-                lastNode = topLeft;
-                
-                p.SetPath(0, points);
-
-                destroyGameObject = true;
-                return;
-            }
-        }*/
     }
 
-    void OriginalPolygon()
+    void GetOriginalPollygonColliderNodes()
     {
         foreach (Vector2 vector in p.points) points.Add(vector);
 
@@ -121,10 +92,7 @@ public class texte : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.black;
-        Gizmos.DrawSphere(detection, 0.09f);
-
         Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(detection2, 0.09f);
+        Gizmos.DrawSphere(detection, 0.1f);
     }
 }
